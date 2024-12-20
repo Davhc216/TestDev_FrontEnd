48b16c1a7e54101f31bb9f00252dc2d9fefb8871
David Hoyos <carlosdavidhc2611@gmail.com>
2024-12-20 05:41:25 -0500

    Se elimina el elemento 'home', ya que este no se ve necesario para la funcionalidad del proyecto. Además de eso se corrigen los html y scss de los modulos dashboard y tareas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirección inicial a 'home'
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Redirección a 'dashboard'
   { path: 'dashboard', loadChildren: () => import('./modulos/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'tareas', loadChildren: () => import('./modulos/tareas/tareas.module').then(m => m.TareasModule) },
+  { path: '**', redirectTo: 'dashboard' } // Manejo de rutas no encontradas
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
